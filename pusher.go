2b87067889b2404e344822c332d6b5ccc7158ae1
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-11-28 20:54:36 +0100

    remove opinionated daemon support api and provide Start + Stop api (#25)

---
+++ pusher.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2016-2020, Jan Cajthaml <jan.cajthaml@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package actorsystem
+
+import (
+  "fmt"
+  "runtime"
+  "time"
+
+  "github.com/pebbe/zmq4"
+)
+
+type Pusher struct {
+  host   string
+  ctx    *zmq4.Context
+  Data   chan string
+  socket *zmq4.Socket
+}
+
+func NewPusher(host string) Pusher {
+  return Pusher{
+    host: host,
+    Data: make(chan string),
+  }
+}
+
+func (s *Pusher) Stop() {
+  if s == nil {
+    return
+  }
+  if s.socket != nil {
+    s.socket.Close()
+  }
+  if s.ctx != nil {
+    for s.ctx.Term() != nil {}
+  }
+  s.socket = nil
+  s.ctx = nil
+}
+
+func (s *Pusher) Start() {
+  var chunk   string
+  var err     error
+
+  runtime.LockOSThread()
+  defer func() {
+    recover()
+    runtime.UnlockOSThread()
+  }()
+
+  s.ctx, err = zmq4.NewContext()
+  if err != nil {
+    return
+  }
+
+pushCreation:
+  s.socket, err = s.ctx.NewSocket(zmq4.PUSH)
+  if err != nil && err.Error() == "resource temporarily unavailable" {
+    select {
+    case <-time.After(100 * time.Millisecond):
+      goto pushCreation
+    }
+  } else if err != nil {
+    return
+  }
+  s.socket.SetConflate(false)
+  s.socket.SetImmediate(true)
+  s.socket.SetSndhwm(0)
+  s.socket.SetLinger(0)
+
+pushConnection:
+  err = s.socket.Connect(fmt.Sprintf("tcp://%s:%d", s.host, 5562))
+  if err != nil && (err == zmq4.ErrorSocketClosed || err == zmq4.ErrorContextClosed || err == zmq4.ErrorNoSocket) {
+    return
+  } else if err != nil {
+    select {
+    case <-time.After(100 * time.Millisecond):
+      goto pushConnection
+    }
+  }
+
+loop:
+  select {
+  case chunk = <-s.Data:
+    if chunk == "" {
+      goto loop
+    }
+    _, err = s.socket.Send(chunk, 0)
+    if err != nil {
+      goto eos
+    }
+  }
+  goto loop
+
+eos:
+  return
+}
---
+++ subber.go
@@ -0,0 +1,111 @@
+// Copyright (c) 2016-2020, Jan Cajthaml <jan.cajthaml@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package actorsystem
+
+import (
+  "fmt"
+  "runtime"
+  "time"
+
+  "github.com/pebbe/zmq4"
+)
+
+type Subber struct {
+  host   string
+  topic  string
+  ctx    *zmq4.Context
+  Data   chan string
+  socket *zmq4.Socket
+}
+
+func NewSubber(host string, topic string) Subber {
+  return Subber{
+    host:  host,
+    topic: topic,
+    Data:  make(chan string),
+  }
+}
+
+func (s *Subber) Stop() {
+  if s == nil {
+    return
+  }
+  if s.socket != nil {
+    s.socket.Close()
+  }
+  if s.ctx != nil {
+    for s.ctx.Term() != nil {}
+  }
+  s.socket = nil
+  s.ctx = nil
+}
+
+func (s *Subber) Start() {
+  var chunk   string
+  var err     error
+
+  runtime.LockOSThread()
+  defer func() {
+    recover()
+    runtime.UnlockOSThread()
+  }()
+
+  s.ctx, err = zmq4.NewContext()
+  if err != nil {
+    return
+  }
+
+subCreation:
+  s.socket, err = s.ctx.NewSocket(zmq4.SUB)
+  if err != nil && err.Error() == "resource temporarily unavailable" {
+    select {
+    case <-time.After(100 * time.Millisecond):
+      goto subCreation
+    }
+  } else if err != nil {
+    return
+  }
+  s.socket.SetConflate(false)
+  s.socket.SetImmediate(true)
+  s.socket.SetRcvhwm(0)
+  s.socket.SetLinger(0)
+
+subConnection:
+  err = s.socket.Connect(fmt.Sprintf("tcp://%s:%d", s.host, 5561))
+  if err != nil && (err == zmq4.ErrorSocketClosed || err == zmq4.ErrorContextClosed || err == zmq4.ErrorNoSocket) {
+    return
+  } else if err != nil {
+    select {
+    case <-time.After(100 * time.Millisecond):
+      goto subConnection
+    }
+  }
+
+  if err = s.socket.SetSubscribe(s.topic + " "); err != nil {
+    return
+  }
+  defer s.socket.SetUnsubscribe(s.topic + " ")
+
+loop:
+  chunk, err = s.socket.Recv(0)
+  if err != nil && (err == zmq4.ErrorSocketClosed || err == zmq4.ErrorContextClosed || err == zmq4.ErrorNoSocket) {
+    goto eos
+  }
+  s.Data <- chunk
+  goto loop
+
+eos:
+  return
+}
