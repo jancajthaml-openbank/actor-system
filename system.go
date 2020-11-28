3d178829da8434cf977727c44c27253504f52e3f
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-11-28 20:52:18 +0100

    remove opinionated daemon support api and provide Start + Stop api

--- system.go
+++ system.go
@@ -17,11 +17,8 @@ package actorsystem
 import (
 	"context"
 	"fmt"
-	"runtime"
 	"strings"
 	"time"
-
-	zmq "github.com/pebbe/zmq4"
 )
 
 // ProcessMessage is a function signature definition for remote message processing
@@ -30,178 +27,36 @@ type ProcessMessage func(msg string, to Coordinates, from Coordinates)
 // System provides support for graceful shutdown
 type System struct {
 	Name      string
-	ctx       context.Context
-	cancel    context.CancelFunc
-	done      chan interface{}
-	IsReady   chan interface{}
-	CanStart  chan interface{}
 	actors    *actorsMap
 	onMessage ProcessMessage
 	host      string
-	publish   chan string
-	receive   chan string
+	push      Pusher
+	sub       Subber
+	ctx       context.Context
+	cancel    context.CancelFunc
 }
 
 // New returns new actor system fascade
-func New(parentCtx context.Context, name string, lakeHostname string) (System, error) {
+func New(name string, lakeHostname string) (System, error) {
 	if lakeHostname == "" {
 		return System{}, fmt.Errorf("invalid lake hostname")
 	}
 	if name == "" || name == "[" {
 		return System{}, fmt.Errorf("invalid system name")
 	}
-	ctx, cancel := context.WithCancel(parentCtx)
+	ctx, cancel := context.WithCancel(context.Background())
 	return System{
-		Name:     name,
-		ctx:      ctx,
-		cancel:   cancel,
-		done:     make(chan interface{}),
-		IsReady:  make(chan interface{}),
-		CanStart: make(chan interface{}),
+		Name:   name,
+		ctx:    ctx,
+		cancel: cancel,
 		actors: &actorsMap{
 			underlying: make(map[string]*Actor),
 		},
 		host:      lakeHostname,
 		onMessage: func(msg string, to Coordinates, from Coordinates) {},
-		publish:   make(chan string),
-		receive:   make(chan string),
+		push:      NewPusher(lakeHostname),
+		sub:       NewSubber(lakeHostname, name),
 	}, nil
-}
-
-func (s *System) workPush() {
-	var (
-		chunk   string
-		channel *zmq.Socket
-		err     error
-	)
-
-	runtime.LockOSThread()
-	defer func() {
-		recover()
-		s.Stop()
-		runtime.UnlockOSThread()
-	}()
-
-	ctx, err := zmq.NewContext()
-	if err != nil {
-		return
-	}
-
-	go func() {
-		<-s.Done()
-		ctx.Term()
-	}()
-
-pushCreation:
-	channel, err = ctx.NewSocket(zmq.PUSH)
-	if err != nil && err.Error() == "resource temporarily unavailable" {
-		select {
-		case <-time.After(100 * time.Millisecond):
-			goto pushCreation
-		}
-	} else if err != nil {
-		return
-	}
-	channel.SetConflate(false)
-	channel.SetImmediate(true)
-	channel.SetSndhwm(0)
-	defer channel.Close()
-
-pushConnection:
-	err = channel.Connect(fmt.Sprintf("tcp://%s:%d", s.host, 5562))
-	if err != nil && (err == zmq.ErrorSocketClosed || err == zmq.ErrorContextClosed || zmq.AsErrno(err) == zmq.ETERM) {
-		return
-	} else if err != nil {
-		select {
-		case <-time.After(100 * time.Millisecond):
-			goto pushConnection
-		}
-	}
-
-loop:
-	select {
-	case chunk = <-s.publish:
-		if chunk == "" {
-			goto loop
-		}
-		_, err = channel.Send(chunk, 0)
-		if err != nil {
-			goto eos
-		}
-	}
-	goto loop
-
-eos:
-	s.Stop()
-	return
-}
-
-func (s *System) workSub() {
-	var (
-		chunk   string
-		channel *zmq.Socket
-		err     error
-	)
-
-	runtime.LockOSThread()
-	defer func() {
-		recover()
-		s.Stop()
-		runtime.UnlockOSThread()
-	}()
-
-	ctx, err := zmq.NewContext()
-	if err != nil {
-		return
-	}
-
-	go func() {
-		<-s.Done()
-		ctx.Term()
-	}()
-
-subCreation:
-	channel, err = ctx.NewSocket(zmq.SUB)
-	if err != nil && err.Error() == "resource temporarily unavailable" {
-		select {
-		case <-time.After(100 * time.Millisecond):
-			goto subCreation
-		}
-	} else if err != nil {
-		return
-	}
-	channel.SetConflate(false)
-	channel.SetImmediate(true)
-	channel.SetRcvhwm(0)
-	defer channel.Close()
-
-subConnection:
-	err = channel.Connect(fmt.Sprintf("tcp://%s:%d", s.host, 5561))
-	if err != nil && (err == zmq.ErrorSocketClosed || err == zmq.ErrorContextClosed || zmq.AsErrno(err) == zmq.ETERM) {
-		return
-	} else if err != nil {
-		select {
-		case <-time.After(100 * time.Millisecond):
-			goto subConnection
-		}
-	}
-
-	if err = channel.SetSubscribe(s.Name + " "); err != nil {
-		return
-	}
-	defer channel.SetUnsubscribe(s.Name + " ")
-
-loop:
-	chunk, err = channel.Recv(0)
-	if err != nil && (err == zmq.ErrorSocketClosed || err == zmq.ErrorContextClosed || zmq.AsErrno(err) == zmq.ETERM) {
-		goto eos
-	}
-	s.receive <- chunk
-	goto loop
-
-eos:
-	s.Stop()
-	return
 }
 
 // RegisterOnMessage register callback on message receive
@@ -221,15 +76,11 @@ func (s *System) RegisterActor(ref *Actor, initialReaction func(interface{}, Con
 	if exists {
 		return
 	}
-
 	ref.React(initialReaction)
 	s.actors.Store(ref.Name, ref)
-
 	go func() {
 		for {
 			select {
-			case <-s.Done():
-				return
 			case p := <-ref.Backlog:
 				ref.Receive(p)
 			case <-ref.Exit:
@@ -237,7 +88,6 @@ func (s *System) RegisterActor(ref *Actor, initialReaction func(interface{}, Con
 			}
 		}
 	}()
-
 	return
 }
 
@@ -274,96 +124,8 @@ func (s *System) SendMessage(msg string, to Coordinates, from Coordinates) {
 	if to.Region == from.Region {
 		s.onMessage(msg, to, from)
 	} else {
-		s.publish <- (to.Region + " " + from.Region + " " + to.Name + " " + from.Name + " " + msg)
+		s.push.Data <- (to.Region + " " + from.Region + " " + to.Name + " " + from.Name + " " + msg)
 	}
-}
-
-// WaitReady wait for daemon to be ready within given deadline
-func (s *System) WaitReady(deadline time.Duration) (err error) {
-	if s.IsCanceled() {
-		return
-	}
-	defer func() {
-		if e := recover(); e != nil {
-			switch x := e.(type) {
-			case string:
-				err = fmt.Errorf(x)
-			case error:
-				err = x
-			default:
-				err = fmt.Errorf("actor-system %s unknown panic", s.Name)
-			}
-		}
-	}()
-
-	ticker := time.NewTicker(deadline)
-	select {
-	case <-s.IsReady:
-		ticker.Stop()
-		err = nil
-		return
-	case <-ticker.C:
-		err = fmt.Errorf("actor-system %s was not ready within %v seconds", s.Name, deadline)
-		return
-	}
-}
-
-// WaitStop cancels context
-func (s *System) WaitStop() {
-	if s == nil {
-		return
-	}
-	<-s.done
-}
-
-// GreenLight signals daemon to start work
-func (s *System) GreenLight() {
-	if s.IsCanceled() {
-		return
-	}
-	s.CanStart <- nil
-}
-
-// MarkDone signals daemon is finished
-func (s *System) MarkDone() {
-	if s == nil {
-		return
-	}
-	close(s.done)
-}
-
-// IsCanceled returns if daemon is done
-func (s *System) IsCanceled() bool {
-	if s == nil {
-		return false
-	}
-	return s.ctx.Err() != nil
-}
-
-// MarkReady signals daemon is ready
-func (s *System) MarkReady() {
-	if s == nil {
-		return
-	}
-	s.IsReady <- nil
-}
-
-// Done cancel channel
-func (s *System) Done() <-chan struct{} {
-	if s == nil {
-		stub := make(chan struct{})
-		close(stub)
-		return stub
-	}
-	return s.ctx.Done()
-}
-
-// Stop cancels context
-func (s *System) Stop() {
-	if s == nil {
-		return
-	}
-	s.cancel()
 }
 
 func (s *System) handshake() {
@@ -377,22 +139,30 @@ func (s *System) handshake() {
 
 	for {
 		select {
-		case <-s.Done():
+		case <-s.ctx.Done():
 			return
-		case data := <-s.receive:
+		case data := <-s.sub.Data:
 			if data != pingMessage {
 				stash = append(stash, data)
 				continue
 			}
 			ticker.Stop()
 			for _, data := range stash {
-				s.receive <- data
+				s.sub.Data <- data
 			}
 			return
 		case <-ticker.C:
-			s.publish <- pingMessage
+			s.push.Data <- pingMessage
 		}
 	}
+}
+
+func (s *System) Stop() {
+	if s == nil {
+		return
+	}
+	s.cancel()
+	<-s.ctx.Done()
 }
 
 // Start handles everything needed to start actor-system
@@ -400,49 +170,47 @@ func (s *System) Start() {
 	if s == nil {
 		return
 	}
-	go s.workPush()
-	go s.workSub()
-
-	s.handshake()
-	s.MarkReady()
-
-	select {
-	case <-s.CanStart:
-		break
-	case <-s.Done():
-		s.Stop()
-		s.MarkDone()
-		return
-	}
 
 	go func() {
-		for {
-			select {
-			case message := <-s.receive:
-				parts := strings.SplitN(message, " ", 5)
-				if len(parts) < 4 {
-					continue
-				}
-				s.onMessage(
-					parts[4],
-					Coordinates{
-						Name:   parts[2],
-						Region: parts[0],
-					},
-					Coordinates{
-						Name:   parts[3],
-						Region: parts[1],
-					},
-				)
-			case <-s.Done():
-				for actorName := range s.actors.underlying {
-					s.UnregisterActor(actorName)
-				}
-				s.MarkDone()
-				return
-			}
+		s.push.Start()
+		s.cancel()
+	}()
+	defer s.push.Stop()
+
+	go func() {
+		s.sub.Start()
+		s.cancel()
+	}()
+	defer s.sub.Stop()
+
+	s.handshake()
+
+	defer func() {
+		for actorName := range s.actors.underlying {
+			s.UnregisterActor(actorName)
 		}
 	}()
 
-	s.WaitStop()
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case message := <-s.sub.Data:
+			parts := strings.SplitN(message, " ", 5)
+			if len(parts) < 4 {
+				continue
+			}
+			s.onMessage(
+				parts[4],
+				Coordinates{
+					Name:   parts[2],
+					Region: parts[0],
+				},
+				Coordinates{
+					Name:   parts[3],
+					Region: parts[1],
+				},
+			)
+		}
+	}
 }
--- system_test.go
+++ system_test.go
@@ -1,101 +1,15 @@
 package actorsystem
 
-import (
-	"context"
-	"runtime"
-	"testing"
-
-	zmq "github.com/pebbe/zmq4"
-)
-
-func relay(ctx context.Context, cancel context.CancelFunc) {
-	runtime.LockOSThread()
-	defer func() {
-		recover()
-		cancel()
-		runtime.UnlockOSThread()
-	}()
-
-	var (
-		chunk string
-		pull  *zmq.Socket
-		pub   *zmq.Socket
-		err   error
-	)
-
-	zCtx, err := zmq.NewContext()
-	if err != nil {
-		panic(err.Error())
-	}
-	defer zCtx.Term()
-
-	pull, err = zCtx.NewSocket(zmq.PULL)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer pull.Close()
-
-	pub, err = zCtx.NewSocket(zmq.PUB)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer pub.Close()
-
-	err = pull.Bind("tcp://127.0.0.1:5562")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	err = pub.Bind("tcp://127.0.0.1:5561")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			chunk, err = pull.Recv(0)
-			if err != nil {
-				return
-			}
-			_, err = pub.Send(chunk, 0)
-			if err != nil {
-				return
-			}
-		}
-	}
-}
-
-func init() {
-	masterContext, masterCancel := context.WithCancel(context.Background())
-	go relay(masterContext, masterCancel)
-}
+import "testing"
 
 func TestStartStop(t *testing.T) {
-	masterContext, masterCancel := context.WithCancel(context.Background())
-	defer masterCancel()
+	system, _ := New("test", "127.0.0.1")
 
-	system, _ := New(masterContext, "test", "127.0.0.1")
-
-	go system.Start()
-	<-system.IsReady
-	system.GreenLight()
+	ack := make(chan bool)
+	go func() {
+		system.Start()
+		close(ack)
+	}()
 	system.Stop()
-	system.WaitStop()
-}
-
-func TestStopOnContextCancel(t *testing.T) {
-	masterContext, masterCancel := context.WithCancel(context.Background())
-	defer masterCancel()
-
-	ctx, cancel := context.WithCancel(masterContext)
-	system, _ := New(ctx, "test", "127.0.0.1")
-
-	go system.Start()
-	<-system.IsReady
-	system.GreenLight()
-	cancel()
-	system.WaitStop()
+	<-ack
 }
